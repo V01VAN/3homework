b2d1aca1416d973038ea2703d44d0e47ce7555a1
V01VAN <149728642+V01VAN@users.noreply.github.com>
2024-05-19 16:28:27 +0300

    доделал дз

--- routes/games.js
+++ routes/games.js
@@ -1,10 +1,10 @@
 // routes/games.js
 const gamesRouter = require("express").Router(); // Создали роутер
 const {sendAllGames,deleteGame,addGameController} = require('../controllers/games')
-const {getAllGames} = require('../middlewares/games')
+const {getAllGames,checkIsTitleInArray,updateGamesArray,updateGamesFile,findGameById,deleteGame} = require('../middlewares/middlewares')
 
-gamesRouter.get("/games",getAllGames, sendAllGames)
-gamesRouter.delete("/games/:id",getAllGames, deleteGame);
-gamesRouter.post("/games", getAllGames,addGameController)
+gamesRouter.get("/games", getAllGames,sendAllGames)
+gamesRouter.delete("/games/:id",getAllGames,checkIsTitleInArray,updateGamesArray,updateGamesFile,findGameById,deleteGame, deleteGame);
+gamesRouter.post("/games", getAllGames,checkIsTitleInArray,updateGamesArray,updateGamesFile,findGameById,deleteGame,addGameController)
 
 module.exports = gamesRouter;
